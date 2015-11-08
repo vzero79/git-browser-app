3d3fc193f6696ff7c862bb3a6620405f32a1c7ee
Leon Ho <vuonghominh@gmail.com>
2015-11-08 21:23:18 +0700

    04_09-Handling Authentication Success and Failure

--- Login.js
+++ Login.js
@@ -67,10 +67,25 @@ class Login extends Component {
       }
     })
     .then((response) => {
+      if(response.status >= 200 && response.status < 300){
+        return response;
+      }
+      throw {
+        badCredentials: response.status == 401,
+        unknownError: response.status != 401
+      }
+    })
+    .then((response) => {
       return response.json();
     })
     .then((results) => {
       console.log(results);
+    })
+    .catch((err) => {
+      console.log('logon failed: ' + err);
+      this.setState(err);
+    })
+    .finally(() => {
       this.setState({showProgress: false});
     });
   }
